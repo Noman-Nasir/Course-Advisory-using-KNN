fab5c947a5c381c58345db51e57a0018910565b3
Noman-Nasir <noman.nasir2016@gmail.com>
2020-02-17 23:42:18 +0500

    Giving Almost Correct Suggestion with k students

--- Assignment1.py
+++ Assignment1.py
@@ -2,6 +2,46 @@ import numpy as np
 import pandas as pd
 from collections import defaultdict
 
+class KNN:
+	def __init__(self, k=1):
+		self.K = k
+
+	def train(self, x, y):
+		self.X = x
+		self.Y = y
+
+	def predict(self, x_pred):
+
+		scores = []
+		for x in self.X:
+			# ['Taken_Courses', 'Grades', 'CGPA', 'SGPA', 'Warning', 'Recommended']
+			if len(set(x[0])&set(x_pred[0])) != 0:
+				score = 0
+				uncommon = len(set(x[0])) + len(set(x_pred[0])) - len(set(x[0])&set(x_pred[0]))*2
+				common = list(set(x[0])&set(x_pred[0]))
+				
+				for c in common:
+					id1 = x_pred[0].index(c)
+					id2 = x[0].index(c)
+					score += (x_pred[1][id1] - x[1][id2])**2
+				score **=0.5
+				score += ((x_pred[2][0] - x[2][0])**2)
+				score += ((x_pred[3][0] - x[3][0])**2)
+				score += ((x_pred[4][0] - x[4][0])**2)
+				score += uncommon
+				scores.append(score)
+			else:
+				scores.append(999)
+			
+		scores = np.array(scores)	
+		min_vals = np.argpartition(scores,self.K)[:self.K]
+		l = [j for sub in list(self.Y[:, 0][min_vals]) for j in sub]
+		d = dict((x, l.count(x)) for x in set(l))
+		d = {k: v for k, v in sorted(d.items(),reverse= True, key=lambda item: item[1])}
+		return d
+	def evaluate(self):
+		pass
+
 # makes a list of values for same keys in diff dictionaries
 def combine_dict(list_of_dictionaries):
     dic = defaultdict(lambda: list())
@@ -21,54 +61,157 @@ def scale(df,col,on_range = True):
         df[col] /= sd
     return
 
-Stud_Data = pd.read_excel('./Course_Advisory_Data.xlsx')
 
-# Extract Col, Semester, RollNo of Students
-columns = Stud_Data.columns.to_list()
-semesters = list(Stud_Data.Semester.unique())
-roll_nos = list(Stud_Data['Sr. No'].unique())
+def adviseMe(classifier, semesterData, course_guide):
+    courses = classifier.predict(semesterData)
+    creditLimit = 19
+    if semesterData[4][0] != 0: # Check Warning Count
+        creditLimit = 15
+    assignedLimit = 0
+    coursesList = list(courses.keys())
+    suggestedCourses = []
+    otherSuggestions = []
+    for c in coursesList:
+        if assignedLimit + course_guide.get(c) <= creditLimit:
+            suggestedCourses.append(c)
+            assignedLimit += course_guide.get(c)
+        elif assignedLimit >= creditLimit:
+            otherSuggestions.append(c)
+    return suggestedCourses,assignedLimit,otherSuggestions
 
-# Dictionary for Course Code and Course Title and Credit Hours
-course_guide = combine_dict([dict(zip(Stud_Data['Course Code'], Stud_Data['Course Title'])),dict(zip(Stud_Data['Course Code'], Stud_Data['Credit Hours']))])
-# Dictionary for Grade and Grade Point
-grade_dictionary = dict(zip(Stud_Data['Grade'], Stud_Data['Grade Point']))
 
-# Set Index To Student's Roll No's
-Stud_Data.set_index('Sr. No',inplace= True)
-# Drop 'Course Title','Grade','Credit Hours' as not used in KNN
-Stud_Data = Stud_Data.drop(labels = ['Course Title','Grade','Credit Hours'],axis = 1)
+if __name__ == "__main__":
 
-# Scale Numerical Cols on Range
-# scale(data, 'CGPA')
-# scale(data, 'SGPA')
-# scale(data, 'Grade Point')
+    Stud_Data = pd.read_excel('./Course_Advisory_Data.xlsx')
 
-# Redifine Data Structure
-modified_data = pd.DataFrame(columns=['Roll_No'],data=roll_nos)
-modified_data.set_index(['Roll_No'],inplace=True)
+    # Extract Col, Semester, RollNo of Students
+    columns = Stud_Data.columns.to_list()
+    semesters = list(Stud_Data.Semester.unique())
+    roll_nos = list(Stud_Data['Sr. No'].unique())
 
-for c in ['Course','Grade_Point','Semester','Warning','SGPA','CGPA']:
-    modified_data[c] = np.empty((len(roll_nos), 0)).tolist()
+    # Dictionary for Course Code and Course Title and Credit Hours
+    course_guide = dict(zip(Stud_Data['Course Title'], Stud_Data['Credit Hours']))
+    # Dictionary for Grade and Grade Point
+    grade_dictionary = dict(zip(Stud_Data['Grade Point'], Stud_Data['Grade']))
 
-roll_nos.sort()
-for roll in roll_nos:
-    for semester in semesters:
-        try:
-            # Convert Each Group To a Separate DataFrame
-            df = pd.DataFrame(Stud_Data.loc[roll])
-            df = df.groupby(['Semester']).get_group(semester)
 
-            # Transfer Data to a new Dataframe
-            modified_data.ix[roll].Semester.append([semester])
-            modified_data.ix[roll].Course.append(list(df['Course Code'].to_list()))
-            modified_data.ix[roll].Grade_Point.append(list(df['Grade Point'].to_list()))
-            modified_data.ix[roll].Warning.append(list([df['Warning'].to_list()[0]]))
-            modified_data.ix[roll].SGPA.append(list([df['SGPA'].to_list()[0]]))
-            modified_data.ix[roll].CGPA.append(list([df['CGPA'].to_list()[0]]))
-        except KeyError:
-            pass
+    # Set Index To Student's Roll No's
+    Stud_Data.set_index('Sr. No',inplace= True)
 
-# Write Data to an excel file
-modified_data.to_pickle('New_Data1.sav')
+    delete_courses = ['Advanced Research Methods','Research in Marketing','MS Thesis - I',
+            'MS Thesis - II','PhD Thesis - I','PhD Thesis - II','PhD Thesis - III',
+            'PhD Thesis - IV','Applied Programming','Research Methodology']
 
-# data = pd.read_pickle('./New_Data_pickle.xlsx')
+    delete_semesters =   [20111,20121,20123,20131]
+
+
+    for line in delete_courses:
+        indexNames = Stud_Data[ Stud_Data['Course Title'] == line ].index
+        Stud_Data.drop(indexNames,inplace=True)
+    for ds in delete_semesters:
+        indexNames = Stud_Data[ Stud_Data['Semester'] == ds ].index
+        Stud_Data.drop(indexNames,inplace=True)
+    grades = [ 'I', 'W', 'FA', 'L1', 'LL', 'L2', 'F/R', 'CN']
+    assignedVal = -1
+    for g in grades:
+        Stud_Data.loc[Stud_Data['Grade'] == g, 'Grade Point'] = assignedVal
+        assignedVal -= 1
+
+    Stud_Data.dropna(inplace= True)
+
+
+
+
+
+    # Scale Numerical Cols on Range
+    # scale(data, 'CGPA')
+    # scale(data, 'SGPA')
+    # scale(data, 'Grade Point')
+
+    semesters = ['Fall 2016','Spring 2017','Summer 2017',
+    'Fall 2017','Spring 2018','Summer 2018','Fall 2018',
+    'Spring 2019','Summer 2019','Fall 2019']
+
+    # Redifine Data Structure
+    modified_data = pd.DataFrame(columns=['Roll_No'],data=roll_nos)
+    modified_data.set_index(['Roll_No'],inplace=True)
+
+    for c in ['Course','Grade_Point','Semester','Warning','SGPA','CGPA']:
+        modified_data[c] = np.empty((len(roll_nos), 0)).tolist()
+
+    roll_nos.sort()
+    for roll in roll_nos:
+        for semester in semesters:
+            try:
+                # Convert Each Group To a Separate DataFrame
+                df = pd.DataFrame(Stud_Data.iloc[roll])
+                df = df.groupby(['Semester']).get_group(semester)
+
+                # Transfer Data to a new Dataframe
+                modified_data.iloc[roll].Semester.append([semester])
+                modified_data.iloc[roll].Course.append(list(df['Course Title'].to_list()))
+                modified_data.iloc[roll].Grade_Point.append(list(df['Grade Point'].to_list()))
+                modified_data.iloc[roll].Warning.append(list([df['Warning'].to_list()[0]]))
+                modified_data.iloc[roll].SGPA.append(list([df['SGPA'].to_list()[0]]))
+                modified_data.iloc[roll].CGPA.append(list([df['CGPA'].to_list()[0]]))
+            except KeyError:
+                pass
+
+    # Write Data to an excel Stud_Datae
+    modified_data.to_pickle('intermediateData.sav')
+
+    data = pd.read_pickle('./New_Data_pickle.xlsx')
+    cols = data.columns.to_list()
+    students = data.index.to_list()
+
+    xyData = pd.DataFrame(columns=['Taken_Courses','Grades','CGPA','SGPA','Warning','Recommended'])
+
+    for student in students:
+        student_data = data.loc[student]
+        for i in range(len(data.loc[student].Semester)-1):
+            # print(student_data['Course Title'])
+            xyData = xyData.append({'Taken_Courses':student_data['Course Title'][i],
+            'Grades':student_data.Grade_Point[i],
+            'CGPA': student_data.CGPA[i],
+            'SGPA':student_data.SGPA[i],
+            'Warning':student_data.Warning[i],
+            'Recommended':student_data['Course Title'][i+1]} ,
+            ignore_index=True)
+            if len(student_data['Course Title'][i]) != len(student_data.Grade_Point[i]):
+                print(len(student_data['Course Title'][i]),len(student_data.Grade_Point[i]))
+
+
+    xyData.to_pickle('xydata.xlsx')
+
+
+
+
+    ratio = 0.8
+    size = int(len(xyData) * ratio)
+    x_train = xyData.values[:size,:5]
+    y_train = xyData.values[:size,5:]
+
+    # print(x_train.shape,y_train.shape)
+
+    x_test = xyData.values[size:,:5]
+    y_test = xyData.values[size:,5:]
+
+    # print(x_test.shape,y_test.shape)
+
+
+    k = 7
+    r = 1000
+    classifier = KNN(k)
+    classifier.train(x_train,y_train)
+
+    cour, lim, other = adviseMe(classifier,x_test[r], course_guide)
+
+    for idx,y in enumerate(x_test[r][0]):
+        print(y,grade_dictionary.get(x_test[r][1][idx]))
+
+    print('\nWarning Count : ',x_test[r][4][0])
+    print('\n\n')
+
+    print(*cour, '\nCredit Hours : '+str(lim), sep= '\n')
+
+    print('\n\n\n*********Other Possible Suggestions****************\n' ,*other, sep= '\n')
